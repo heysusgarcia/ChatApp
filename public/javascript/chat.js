cec8e558f48e5f78eea1bfb8bd3b874637dc22e2
Jesus Garcia <heysusgarcia@gmail.com>
2014-08-11 20:17:52 -0700

    added moment.js timestamp & css styling

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,24 +1,29 @@
 (function(root) {
   var C = root.C = (root.C || {});
-  
+
   var Chat = C.Chat = function(socket, room) {
     this.socket = socket;
     this.room = room;
-    
+
     this.socket.on("nicknameChangeResult", function (results) {
-      var $result = $("<div>").text(results.message);
-      $(".messages").append($result);
+      var $result = $("<div class='changed'>").text(results.message);
+      $(".messages").prepend($result);
+      $('textarea').val('');
     });
-    
+
     this.socket.on("message", function (message) {
       console.log("message?", message)
       var $message = $("<div class='bg-info'>").text(message.text);
-      $(".messages").append($message);
+      var timeStamp = $("<i class='timestamp pull-right'>").text(' ' + moment().format('LT'));
+      $message.append(timeStamp);
+      $(".messages").prepend($message);
+      $('textarea').val('');
     });
-    
+
     this.socket.on("roomChangeResult", function (result) {
-      var $message = $("<div>").text(result.text);
-      $(".messages").append($message);
+      var $message = $("<div class='changed'>").text(result.text);
+      $(".messages").prepend($message);
+      $('textarea').val('');
       this.room = result.roomname;
     });
     this.socket.on("roomList", function (list) {
@@ -26,23 +31,23 @@
       for (var i=0; i < list.length; i++) {
         var $li = $("<li>");
         $li.text(list[i]);
-        $list.append($li);
+        $list.prepend($li);
       }
       $(".room-list").html($list);
-      $(".room-list").prepend("<h1>Current Users</h1>")
+      $(".room-list").prepend("<h1 style='margin-top: 0px'>Current Users</h1>")
     })
   };
-  
+
   Chat.prototype.sendMessage = function(message) {
     this.socket.emit("message", { text: message });
   };
-  
+
   Chat.prototype.changeNickname = function (nickname) {
     this.socket.emit("nicknameChangeRequest", nickname);
   };
-  
+
   Chat.prototype.changeRoom = function(room) {
     this.socket.emit("handleRoomChangeRequests", room);
   };
-  
+
 })(this);
--- public/javascript/chat_ui.js
+++ public/javascript/chat_ui.js
@@ -10,27 +10,25 @@ var getMessage = function() {
 
 var sendMessage = function(message) {
   Chat.sendMessage(message);
-  // var $message = $('<div>').text(message);
-  // $(".messages").append($message);
 };
 
 var processCommand = function (message) {
   if (message.indexOf("nick") === 1) {
     var nickname = message.split(" ")[1];
-    
+
     Chat.changeNickname(nickname);
   } else if (message.indexOf("join") === 1) {
     var room = message.split(" ")[1];
     Chat.changeRoom(room);
   } else {
-    $(".messages").append("<div>command not recognize</div>");
+    $(".messages").prepend("<div class='warning'>command not recognized</div>");
   }
 }
 
 $(document).ready(function () {
   $('form').on("submit", function(event) {
     event.preventDefault();
-    
+
     var message = getMessage();
     if (message.charAt(0) === "/") {
       processCommand(message);
